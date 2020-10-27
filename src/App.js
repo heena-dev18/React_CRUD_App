89fecf36aaf96b529c3c9505f55e956bf96ace36
unknown <you@example.com>
2020-10-27 09:26:56 +0530

    first commit reactJs WebApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router ,Route,Switch} from "react-router-dom";
+import NotFound from './components/pages/NotFound';
+import AddUser from "./components/layout/users/AddUser";
+import EditUsers from "./components/layout/users/EditUser";
+import User from './components/layout/users/User';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+       <Navbar/>
+       <Switch>
+         <Route exact path="/" component ={Home}/>
+         <Route exact path="/about" component={About}/>
+         <Route exact path="/contact" component={Contact}/>
+         <Route exact path="/users/adduser" component={AddUser} />
+         <Route exact path="/users/edit/:id" component={EditUsers}/>
+         <Route exact path="/users/:id" component={User} />
+         <Route component={NotFound}/>
+       </Switch>
     </div>
+    </Router>
   );
 }
 
 export default App;
+    
+
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <div className="not-found">
+             <div>
+            <h1 className="display-1">Page Not Found</h1>
+            </div>
+        </div>
+       
+    )
+};
+
+export default NotFound;
